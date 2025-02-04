7e9877be945b25e127ad8422b364d4797f3f9456
hungndweq <thang7781@gmail.com>
2025-02-04 11:07:41 +0700

    Java bean to respone

--- src/main/java/com/javaweb/api/BuildingAPI.java
+++ src/main/java/com/javaweb/api/BuildingAPI.java
@@ -1,17 +1,32 @@
 package com.javaweb.api;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import com.javaweb.object.BuildingDTO;
 
 @Controller
 public class BuildingAPI {
 	@RequestMapping(value = "/api/building/", method = RequestMethod.GET)
-	private void getBuilding(@RequestParam(value = "name") String name,
-							 @RequestParam(value = "numberOfBasement") Integer numberOfBasement) {
-		System.out.print(name);
-		System.out.print(numberOfBasement);
+	@ResponseBody
+	private BuildingDTO getBuilding(@RequestParam(value = "name",required = false) String name,
+							 @RequestParam(value = "numberOfBasement", required = false) Integer numberOfBasement,
+							 @RequestParam(value = "ward", required = false) String ward) {
+		BuildingDTO getResult = new BuildingDTO();
+		getResult.setName(name);
+		getResult.setNumberOfBasement(numberOfBasement);
+		getResult.setWard(ward);
+		return getResult;
+
+	}
+	
+	@RequestMapping(value = "/api/building/", method = RequestMethod.POST)
+	private BuildingDTO postBuilding(@RequestBody BuildingDTO buildingDTO) {
+		return buildingDTO;
 
 	}
 }
